dd1c7d62f0762eb963bc0b692ffcb2534191beb8
firattamur <ftamur16@ku.edu.tr>
2023-04-04 03:39:03 +0300

    :bug: fix: fix s3 upload image function

--- app/service/awss3.py
+++ app/service/awss3.py
@@ -2,6 +2,7 @@ import uuid
 from io import BytesIO
 from typing import Optional
 
+from fastapi import UploadFile
 from PIL import Image
 
 from app.core.config import AWS_S3_BUCKET_NAME
@@ -19,48 +20,46 @@ class AWSS3:
         """
         self.bucket_name = AWS_S3_BUCKET_NAME
 
-    def upload_image(self, image: Image) -> Optional[str]:
+    def upload_image(self, image: UploadFile) -> Optional[str]:
         """
+        Uploads an image to S3 bucket and returns S3 key.
 
-        Upload image to S3 bucket and return S3 key.
         :param image: Image to upload
-        :param s3_key: S3 key of image
-
-        :return: S3 key of image
-
+        :return: S3 key of image, or None if upload failed
         """
 
         s3_key = f"original/{str(uuid.uuid4())}.jpg"
 
-        with BytesIO() as output:
-            try:
-                image.save(output, format="JPEG")
+        try:
+            with BytesIO() as output:
+                image.file.seek(0)
+                output.write(image.file.read())
                 output.seek(0)
+
                 AWSClients.s3.upload_fileobj(output, self.bucket_name, s3_key)
 
-                return s3_key
+            return s3_key
 
-            except Exception as e:
-                print(f"Error uploading image to S3: {e}")
-
-                return None
+        except Exception as e:
+            print(f"Error uploading image to S3: {e}")
+            return None
 
     def download_image(self, s3_key: str) -> Optional[Image.Image]:
         """
-        Download image from S3 bucket.
+        Downloads an image from S3 bucket and returns it as a PIL Image object.
+
         :param s3_key: S3 key of image
-        :return: Image
+        :return: Image object, or None if download failed
         """
 
-        with BytesIO() as output:
-            try:
+        try:
+            with BytesIO() as output:
                 AWSClients.s3.download_fileobj(self.bucket_name, s3_key, output)
                 output.seek(0)
                 image = Image.open(output)
 
-                return image
+            return image
 
-            except Exception as e:
-                print(f"Error downloading image from S3: {e}")
-
-                return None
+        except Exception as e:
+            print(f"Error downloading image from S3: {e}")
+            return None
